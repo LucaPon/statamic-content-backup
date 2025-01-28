1567c99900cbea97c4d5e51c6dda4d9cd6d35080
LucaPon <luca.ponte95@gmail.com>
2025-01-28 23:58:46 +0100

    feat: adding support for database stored content

--- src/Http/Services/BackupService.php
+++ src/Http/Services/BackupService.php
@@ -2,43 +2,113 @@
 
 namespace LucaPon\StatamicContentBackup\Http\Services;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
 use Statamic\Facades\Stache;
 use ZipArchive;
 
 class BackupService
 {
     private $tempFolderName = "temp";
-    private $config = "statamic-content-backup.include_files";
+    private $databaseBasePath = 'database';
+    private $filesBasePath = 'files';
 
     public function createBackup(): string
     {
-        $includeFiles = config()->get($this->config);
-        $backupFileName = date('Ymd') . '_' . env('APP_NAME') . '.zip';
+        $backupFileName = $this->generateBackupFileName();
+
+        $includeTables = config()->get('statamic-content-backup.include_tables');
+        $includeFiles = config()->get('statamic-content-backup.include_files');
+
+        $tempFolder = $this->getTempFolder();
+        $backupPath = $tempFolder . '/' . $backupFileName;
 
         $zip = new ZipArchive();
-        $backupPath = storage_path() . DIRECTORY_SEPARATOR . $backupFileName;
-        $zip->open($backupPath, ZipArchive::CREATE | ZipArchive::OVERWRITE);
-
-        foreach ($includeFiles as $file) {
-            $fileName = base_path($file);
-            if(File::exists($fileName)) {
-                $this->addToZip($fileName, $zip);
-            }
+        if ($zip->open($backupPath, ZipArchive::CREATE | ZipArchive::OVERWRITE) !== true) {
+            throw new \Exception('Error creating backup file');
         }
 
-        if(!$zip->close()){
-            throw new \Exception('Error creating backup');
+        $this->backupFiles($zip, $includeFiles);
+        $this->backupDatabaseTables($zip, $includeTables);
+
+        if (!$zip->close()) {
+            throw new \Exception('Error closing backup file');
         }
 
         return $backupPath;
 
     }
 
-    public function restoreBackup($backupPath): void
+    private function generateBackupFilename(): string
+    {
+        $date = date('Ymd');
+        $time = date('His');
+        $appName = env('APP_NAME');
+        $format = config('statamic-content-backup.backup_filename_format');
+        return str_replace(['{date}', '{time}', '{appName}'], [$date, $time, $appName], $format) . '.zip';
+    }
+
+    private function backupFiles($zip, $includeFiles): void
+    {
+        foreach ($includeFiles as $file) {
+            $filePath = base_path($file);
+            if(File::exists($filePath)) {
+                $this->addToZip($filePath, $zip, $this->filesBasePath . $file);
+            }else {
+                Log::warning("File or directory not found: $filePath");
+            }
+        }
+    }
+
+    private function backupDatabaseTables($zip, $includeTables): void{
+
+        if(!empty($includeTables)){
+
+            $dbDumper = $this->getDbDumper();
+
+            foreach ($includeTables as $table) {
+                $tableDumpPath = $this->getTempFolder() . '/' . $table . '.sql';
+
+                $dbDumper->includeTables([$table])
+                    ->dumpToFile($tableDumpPath);
+
+                $this->addToZip($tableDumpPath, $zip, $this->databaseBasePath . '/' . $table . '.sql');
+            }
+        }
+    }
+
+    private function getDbDumper(): \Spatie\DbDumper\DbDumper
     {
 
-        $includeFiles = config()->get($this->config);
+        $databaseConnection = config()->get('database.default');
+        $databaseDriver = config()->get('database.connections.' . $databaseConnection . '.driver');
+
+        if($databaseDriver === 'mysql') {
+
+            $dbConfigPrefix = 'database.connections.' . $databaseConnection;
+            $dbName = config()->get( $dbConfigPrefix . '.database');
+            $dbHost = config()->get( $dbConfigPrefix . '.host');
+            $dbPort = config()->get( $dbConfigPrefix . '.port');
+            $userName = config()->get( $dbConfigPrefix . '.username');
+            $password = config()->get( $dbConfigPrefix . '.password');
+
+            return \Spatie\DbDumper\Databases\MySql::create()
+                ->setDbName($dbName)
+                ->setHost($dbHost)
+                ->setPort($dbPort)
+                ->setUserName($userName)
+                ->setPassword($password);
+
+        }else {
+            throw new \Exception('Database driver not supported');
+        }
+    }
+
+    public function restoreBackup($backupPath): void
+    {
+        $includeTables = config()->get('statamic-content-backup.include_tables');
+        $includeFiles = config()->get('statamic-content-backup.include_files');
 
         $zip = new ZipArchive();
         if(!$zip->open($backupPath)){
@@ -51,7 +121,7 @@ class BackupService
             $zip->close();
 
             foreach ($includeFiles as $file) {
-                $tempFile = $tempFolder . DIRECTORY_SEPARATOR . $file;
+                $tempFile = $tempFolder . '/' . $this->filesBasePath . '/' . $file;
                 $oldFile = base_path($file);
                 if(File::exists($tempFile)) {
 
@@ -66,29 +136,38 @@ class BackupService
                 }
             }
 
-            $this->cleanTempFolder();
+            foreach( $includeTables as $table){
+                $tableDumpPath = $tempFolder . '/' . $this->databaseBasePath . '/' . $table . '.sql';
+                if(File::exists($tableDumpPath)) {
+                    DB::unprepared(File::get($tableDumpPath));
+                }
+            }
+
+            $this->cleanup();
 
             //Refresh Statamic Stache
             Stache::refresh();
 
         }catch (\Exception $e) {
-            $this->cleanTempFolder();
+            $this->cleanup();
             throw $e;
         }
     }
 
-    private function addToZip($file, $zip): void
+    private function addToZip($file, $zip, $entryName = null): void
     {
         if (!file_exists($file)) {
             return;
         }
 
+        $entryName = $entryName ?? str_replace(base_path(), '', $file);
+
         if (is_file($file)) {
-            $zip->addFile($file, str_replace(base_path(), '', $file));
+            $zip->addFile($file, $entryName);
         } elseif (is_dir($file)) {
             $files = File::allFiles($file, true);
             foreach ($files as $file) {
-                $this->addToZip($file, $zip);
+                $this->addToZip($file, $zip, $entryName . '/' . $file->getRelativePathname());
             }
         }
     }
@@ -97,18 +176,17 @@ class BackupService
     {
         $tempFolder = storage_path($this->tempFolderName);
         if (!File::exists($tempFolder)) {
-            File::makeDirectory($tempFolder);
+            File::makeDirectory($tempFolder, recursive: true);
         }
 
         return $tempFolder;
     }
 
-    private function cleanTempFolder(): void
+    public function cleanup(): void
     {
-        $tempFolder = storage_path($this->tempFolderName);
+        $tempFolder = $this->getTempFolder();
         if (File::exists($tempFolder)) {
             File::deleteDirectory($tempFolder);
         }
-
     }
 }
