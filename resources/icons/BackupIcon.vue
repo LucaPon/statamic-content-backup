1dfd83098960ecd37ec8e774ec655dc0753731a6
LucaPon <luca.ponte95@gmail.com>
2025-06-09 09:51:36 +0200

    feat: changed backup icon

--- resources/icons/BackupIcon.vue
+++ resources/icons/BackupIcon.vue
@@ -9,12 +9,14 @@
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
-    class="feather feather-pocket"
+    class="feather feather-hard-drive"
   >
+    <line x1="22" y1="12" x2="2" y2="12"></line>
     <path
-      d="M4 3h16a2 2 0 0 1 2 2v6a10 10 0 0 1-10 10A10 10 0 0 1 2 11V5a2 2 0 0 1 2-2z"
+      d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"
     ></path>
-    <polyline points="8 10 12 14 16 10"></polyline>
+    <line x1="6" y1="16" x2="6.01" y2="16"></line>
+    <line x1="10" y1="16" x2="10.01" y2="16"></line>
   </svg>
 </template>
 
